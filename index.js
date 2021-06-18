83db08e20e1ea6cc1ff38bf33a71f887f25d2b8b
pkkeiki <78527585+pkkeiki@users.noreply.github.com>
2021-06-18 15:38:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ ComfyJS.onCommand = (user, command, message, flags, extra) => {
     new gifAlert(user, lurkGif, magicChime, command);
   }
    if (command == "rage") {
-    new gifAlert(rageGif, magicChime, user, command);
+    new gifAlert(user, rageGif, magicChime, command);
   }
   if (command == "welcome") {
     new gifAlert(message, welcomeGif, magicChime, command);
