65a7f7edfd03145d25413989e3ebd63c1f7a0245
pkkeiki <78527585+pkkeiki@users.noreply.github.com>
2021-08-27 13:03:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -97,23 +97,27 @@ const generateTitle = {
 
 const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight
 
-const resizeText = ({ element, parent }) => {
-  let i = 12 // let's start with 12px
-  let overflow = false
-  const maxSize = 128 // very huge text size
+const resizeText = ({ element, elements, minSize = 10, maxSize = 512, step = 1, unit = 'px' }) => {
+  (elements || [element]).forEach(el => {
+    let i = minSize
+    let overflow = false
 
-  while (!overflow && i < maxSize) {
-    element.style.fontSize = `${i}px`
-    overflow = isOverflown(parent)
-    if (!overflow) i++
-  }
+        const parent = el.parentNode
 
-  // revert to last state where no overflow happened:
-  element.style.fontSize = `${i - 1}px`
+    while (!overflow && i < maxSize) {
+        el.style.fontSize = `${i}${unit}`
+        overflow = isOverflown(parent)
+
+      if (!overflow) i += step
+    }
+
+    // revert to last state where no overflow happened
+    el.style.fontSize = `${i - step}${unit}`
+  })
 }
 resizeText({
-  element: document.querySelector('.text-shadows'),
-  parent: document.querySelector('.text-container')
+  elements: document.querySelectorAll('.text-shadows'),
+  step: 0.5
 })
 
 
